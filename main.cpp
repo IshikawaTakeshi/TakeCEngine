7d6970757cb9fb70a2db4c03d38ba35a490ce25a
bluecrown5@ymobile.ne.jp <bluecrown2525@gmail.com>
2024-02-23 03:13:35 +0900

    コミット

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
+#pragma warning(push)
+//C4023の警告を見なかったことにする
+#pragma warning(disable:4023)
 #include <Windows.h>
+#pragma warning(pop)
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	OutputDebugStringA("Hello,DirectX!\n");
