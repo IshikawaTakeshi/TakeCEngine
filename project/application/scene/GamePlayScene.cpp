f5bf5b2e2ee4a301e03dae032d61c7f4e4559834
IshikawaTakeshi <K023G0065@g.neec.ac.jp>
2025-01-26 23:41:56 +0900

    コミット

--- project/application/scene/GamePlayScene.cpp
+++ project/application/scene/GamePlayScene.cpp
@@ -72,7 +72,7 @@ void GamePlayScene::Initialize() {
 	sphereModel_->Initialize(Object3dCommon::GetInstance(), "sphere.obj");
 
 	AnimationModel_ = std::make_unique<Object3d>();
-	AnimationModel_->Initialize(Object3dCommon::GetInstance(), "Animation_Node_03.gltf");
+	AnimationModel_->Initialize(Object3dCommon::GetInstance(), "Animation_Node_00.gltf");
 	AnimationModel_->SetPosition({ 10.0f,0.0f,0.0f });
 	AnimationModel_->SetScale({ 2.0f,2.0f,2.0f });
 	AnimationModel_->GetModel()->GetMesh()->GetMaterial()->SetEnvCoefficient(0.0f);
