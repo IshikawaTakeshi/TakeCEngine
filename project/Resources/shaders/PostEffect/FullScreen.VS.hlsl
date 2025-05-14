c5ba7b5707beb32328b4d9ffe11adacdf374ea4a
IshikawaTakeshi <146906463+IshikawaTakeshi@users.noreply.github.com>
2025-05-14 13:59:42 +0900

    FullScreenVSのRenderTextureのtexcoordを修正

--- project/Resources/shaders/PostEffect/FullScreen.VS.hlsl
+++ project/Resources/shaders/PostEffect/FullScreen.VS.hlsl
@@ -10,7 +10,7 @@ static const float4 kPositions[kNumVertex] = {
 static const float2 kTexCoords[kNumVertex] = {
 	float2(0.0f, 0.0f), //LeftTop
 	float2(2.0f, 0.0f), //RightTop
-	float2(0.0f,-2.0f)  //LeftBottom
+	float2(0.0f, 2.0f)  //LeftBottom
 };
 
 VertexShaderOutput main(uint vertexId : SV_VertexID) {
