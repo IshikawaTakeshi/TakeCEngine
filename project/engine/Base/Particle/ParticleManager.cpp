97f8b49e8ba474b4cf259baf8952c03eab7e35ab
IshikawaTakeshi <K023G0065@g.neec.ac.jp>
2025-12-26 05:55:05 +0900

    コミット

---
+++ project/engine/Base/Particle/ParticleManager.cpp
@@ -0,0 +1,203 @@
+#include "ParticleManager.h"
+#include "base/ImGuiManager.h"
+#include "base/TakeCFrameWork.h"
+#include "engine/3d/Particle/ParticleCommon.h"
+#include <cassert>
+
+//================================================================================================
+// 初期化
+//================================================================================================
+
+void TakeC::ParticleManager::Initialize(ParticleCommon* particleCommon) {
+	emitterAllocator_ = std::make_unique<ParticleEmitterAllocator>();
+	particleCommon_ = particleCommon;
+}
+
+//================================================================================================
+// 更新処理
+//================================================================================================
+
+void TakeC::ParticleManager::Update() {
+	for (auto& [name, particleGroup] : particleGroups_) {
+		particleGroup->Update();
+	}
+}
+
+//================================================================================================
+// ImGuiの更新処理
+//================================================================================================
+
+void TakeC::ParticleManager::UpdateImGui() {
+
+#ifdef _DEBUG
+	ImGui::Begin("ParticleManager");
+	ImGui::Text("ParticleGroup Count : %d", particleGroups_.size());
+	ImGui::Separator();
+	for (const auto& [name, particleGroup] : particleGroups_) {
+
+		if (ImGui::TreeNode(name.c_str())) {
+			particleGroup->UpdateImGui();
+			ImGui::TreePop();
+		}
+	}
+
+	ImGui::End();
+#endif // _DEBUG
+}
+
+//================================================================================================
+// 描画処理
+//================================================================================================
+
+void TakeC::ParticleManager::Draw() {
+	std::unordered_map<BlendState, std::vector<PrimitiveParticle*>> blendGroups;
+	for (auto& [name, group] : particleGroups_) {
+		BlendState state = group->GetPreset().blendState; // BlendMode取得
+		blendGroups[state].push_back(group.get());
+	}
+
+	//各ブレンドモードごとに描画
+	for (auto& [blendState, groups] : blendGroups) {
+
+		if (blendState == BlendState::COUNT) {
+			return;
+		}
+		
+		particleCommon_->PreDraw(blendState);
+
+		for (auto group : groups) {
+			group->Draw();
+		}
+		
+	}
+}
+
+//================================================================================================
+// 終了処理
+//================================================================================================
+
+void TakeC::ParticleManager::Finalize() {
+	particleGroups_.clear();
+}
+
+void TakeC::ParticleManager::UpdatePrimitiveType(const std::string& groupName, PrimitiveType type,const Vector3& param) {
+
+	if (!particleGroups_.contains(groupName)) {
+		assert(false && "ParticleGroup not found! Please check the name.");
+		return;
+	}
+
+	uint32_t newHandle = 0;
+	//指定されたグループのプリミティブタイプを更新
+	switch (type) {
+	case PRIMITIVE_RING:
+		newHandle = TakeCFrameWork::GetPrimitiveDrawer()->GenerateRing(
+			param.x,param.y,particleGroups_.at(groupName)->GetPreset().textureFilePath);
+		break;
+	case PRIMITIVE_PLANE:
+		newHandle = TakeCFrameWork::GetPrimitiveDrawer()->GeneratePlane(
+			param.x, param.y, particleGroups_.at(groupName)->GetPreset().textureFilePath);
+		break;
+	case PRIMITIVE_SPHERE:
+		newHandle = TakeCFrameWork::GetPrimitiveDrawer()->GenerateSphere(
+			param.x, particleGroups_.at(groupName)->GetPreset().textureFilePath);
+		break;
+	case PRIMITIVE_CONE:
+		newHandle = TakeCFrameWork::GetPrimitiveDrawer()->GenerateCone(
+			param.x, param.y,16, particleGroups_.at(groupName)->GetPreset().textureFilePath);
+		break;
+	default:
+		break;
+	}
+	particleGroups_.at(groupName)->SetPrimitiveHandle(newHandle);
+}
+
+//================================================================================================
+// パーティクルグループの生成
+//================================================================================================
+
+void TakeC::ParticleManager::CreateParticleGroup(const std::string& name,const std::string& filePath,PrimitiveType primitiveType) {
+
+	if (particleGroups_.contains(name)) {
+		//既に同名のparticleGroupが存在する場合は生成しない
+		return;
+	}
+
+	//particleGroupの生成
+	std::unique_ptr<PrimitiveParticle> particleGroup = std::make_unique<PrimitiveParticle>(primitiveType);
+	particleGroup->Initialize(particleCommon_, filePath);
+	particleGroups_.insert(std::make_pair(name, std::move(particleGroup)));
+}
+
+void TakeC::ParticleManager::CreateParticleGroup(const std::string& presetJson) {
+	ParticlePreset preset = TakeCFrameWork::GetJsonLoader()->LoadJsonData<ParticlePreset>(presetJson);
+	if(particleGroups_.contains(preset.presetName)) {
+		//既に同名のparticleGroupが存在する場合は生成しない
+		return;
+	}
+
+	//particleGroupの生成
+	std::unique_ptr<PrimitiveParticle> particleGroup = std::make_unique<PrimitiveParticle>(preset.primitiveType);
+	particleGroup->Initialize(particleCommon_, preset.textureFilePath);
+	particleGroup->SetPreset(preset);
+	particleGroups_.insert(std::make_pair(preset.presetName, std::move(particleGroup)));
+}
+
+
+//================================================================================================
+// パーティクルの発生
+//================================================================================================
+
+void TakeC::ParticleManager::Emit(const std::string& name, const Vector3& emitPosition,const Vector3& direction, uint32_t count) {
+
+	//存在しない場合は処理しない
+	if (!particleGroups_.contains(name)) {
+		return;
+	}
+
+	//particleGroupsに発生させたパーティクルを登録させる
+	particleGroups_.at(name)->SpliceParticles(particleGroups_.at(name)->Emit(emitPosition,direction, count));
+}
+
+//================================================================================================
+// エミッター用のハンドルの割り当て
+//================================================================================================
+uint32_t TakeC::ParticleManager::EmitterAllocate() {
+	return emitterAllocator_->Allocate();
+}
+
+//================================================================================================
+// パーティクルグループの開放
+//================================================================================================
+void TakeC::ParticleManager::ClearParticleGroups() {
+	particleGroups_.clear();
+}
+
+//================================================================================================
+// 出現しているパーティクルのクリア
+//================================================================================================
+void TakeC::ParticleManager::ClearParticles() {
+	for (auto& [name, particleGroup] : particleGroups_) {
+		particleGroup->EraseParticle();
+	}
+}
+
+BaseParticleGroup* TakeC::ParticleManager::GetParticleGroup(const std::string& name) {
+
+	if (particleGroups_.contains(name)) {
+		return particleGroups_.at(name).get();
+	}
+
+	assert(false && "ParticleGroup not found! Please check the name.");
+	return nullptr;
+}
+
+void TakeC::ParticleManager::SetPreset(const std::string& name, const ParticlePreset& preset) {
+
+	//存在しない場合は処理しない
+	if (!particleGroups_.contains(name)) {
+		return;
+	}
+	//particleGroupsに発生させたパーティクルを登録させる
+	particleGroups_.at(name)->SetPreset(preset);
+}
---
+++ project/engine/Base/Particle/ParticleManager.h
@@ -0,0 +1,73 @@
+#pragma once
+#include "engine/3d/Particle/Particle3d.h"
+#include "engine/3d/Particle/PrimitiveParticle.h"
+#include "engine/3d/Particle/ParticleEmitterAllocater.h"
+#include "engine/base/BlendModeStateEnum.h"
+#include <list>
+#include <wrl.h>
+#include <unordered_map>
+#include <string>
+#include <memory>
+#include <random>
+
+// 前方宣言
+class ParticleCommon;
+
+//==================================================================================
+// パーティクルマネージャー
+//==================================================================================
+namespace TakeC {
+	class ParticleManager {
+	public:
+		ParticleManager() = default;
+		~ParticleManager() = default;
+
+		// 初期化
+		void Initialize(ParticleCommon* particleCommon);
+		// 更新処理
+		void Update();
+		// ImGuiの更新処理
+		void UpdateImGui();
+		// 描画処理
+		void Draw();
+		// 終了処理
+		void Finalize();
+
+		void UpdatePrimitiveType(const std::string& groupName, PrimitiveType type, const Vector3& param);
+
+		/// <summary>
+		/// パーティクルグループの生成
+		/// </summary>
+		/// <param name="particleCommon">パーティクル共通情報</param>
+		/// <param name="name">グループ名(固有名)</param>
+		/// <param name="filePath">objファイルパス</param>
+		void CreateParticleGroup( const std::string& name,
+			const std::string& filePath, PrimitiveType primitiveType = PRIMITIVE_PLANE);
+
+		//パーティクルグループの生成(JSONから)
+		void CreateParticleGroup(const std::string& presetJson);
+		//パーティクル射出
+		void Emit(const std::string& name, const Vector3& emitPosition, const Vector3& direction, uint32_t count);
+		//エミッター用のハンドルの割り当て
+		uint32_t EmitterAllocate();
+		//パーティクルグループの開放
+		void ClearParticleGroups();
+
+		//出現しているパーティクルのクリア
+		void ClearParticles();
+
+		//パーティクルグループの取得
+		BaseParticleGroup* GetParticleGroup(const std::string& name);
+		//プリセットの設定
+		void SetPreset(const std::string& name, const ParticlePreset& preset);
+
+	private:
+
+		//エミッターアロケータ
+		std::unique_ptr<ParticleEmitterAllocator> emitterAllocator_;
+		//パーティクルグループ
+		std::unordered_map<std::string, std::unique_ptr<PrimitiveParticle>> particleGroups_;
+		//パーティクル共通情報
+		ParticleCommon* particleCommon_ = nullptr;
+	};
+}
