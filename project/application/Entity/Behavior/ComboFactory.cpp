8557f0ee03dc05e058b2ce854ed99472097c1c4c
IshikawaTakeshi <K023G0065@g.neec.ac.jp>
2026-03-10 11:25:41 +0900

    blackboardクラスを実装したい

--- project/application/Entity/Behavior/ComboFactory.cpp
+++ project/application/Entity/Behavior/ComboFactory.cpp
@@ -19,58 +19,15 @@ std::unique_ptr<BehaviorNode> ComboFactory::BuildActionNode(
 std::unique_ptr<BehaviorNode> ComboFactory::BuildConditionNode(
 	const BehaviorNodeData& nodeData, AIBrainSystem* brain) {
 	
-	ConditionType conditionType = StringUtility::StringToEnum<ConditionType>(nodeData.conditionType);
+	const std::string& field = nodeData.field;
+	const std::string& op = nodeData.op;
 	float threshold = nodeData.conditionThreshold;
 	int scoreIndex = nodeData.scoreIndex;
 
 	//条件に応じたラムダ関数を生成
 	std::function<bool(const PlayableCharacterInfo&)> conditionFunc;
 
-	switch (conditionType) {
-	case ConditionType::ENERGY_ABOVE:
-		conditionFunc = [threshold](const PlayableCharacterInfo& info) {
-			return info.energyInfo.energy >= threshold;
-		};
-		break;
-	case ConditionType::HP_BELOW:
-		conditionFunc = [threshold](const PlayableCharacterInfo& info) {
-			return info.health <= threshold;
-		};
-		break;
-	case ConditionType::ON_GROUND:
-		conditionFunc = [](const PlayableCharacterInfo& info) {
-			return info.onGround;
-		};
-		break;
-	case ConditionType::IN_AIR:
-		conditionFunc = [](const PlayableCharacterInfo& info) {
-			return !info.onGround;
-		};
-		break;
-	case ConditionType::DISTANCE_BELOW:
-		conditionFunc = [threshold,brain](const PlayableCharacterInfo& info) {
-			return brain->GetDistanceToTarget() <= threshold;
-		};
-		break;
-	case ConditionType::DISTANCE_ABOVE:
-		conditionFunc = [threshold,brain](const PlayableCharacterInfo& info) {
-			return brain->GetDistanceToTarget() >= threshold;
-		};
-		break;
-	case ConditionType::SCORE_ABOVE:
-		conditionFunc = [threshold, brain, scoreIndex](const PlayableCharacterInfo& info) {
-			return brain->GetAttackScore(scoreIndex) >= threshold;
-		};
-	case ConditionType::NOT_OVERHEATED:
-		conditionFunc = [](const PlayableCharacterInfo& info) {
-			return !info.overHeatInfo.isOverheated;
-			};
-	default:
-		conditionFunc = [](const PlayableCharacterInfo& info) {
-			return true; // デフォルトは常にtrue
-		};
-		break;
-	}
+	if(field == "energy")
 
 	return std::make_unique<ConditionNode>(std::move(conditionFunc),nodeData.name);
 }
